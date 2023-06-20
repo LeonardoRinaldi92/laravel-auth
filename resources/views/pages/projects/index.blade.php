f8b2fdf1d76011eb561aea35dbd304adca750ed5
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-06-20 07:37:20 +0200

    seeder funzionante

--- resources/views/pages/projects/index.blade.php
+++ resources/views/pages/projects/index.blade.php
@@ -1,13 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    ciao
-    
-</body>
-</html>
+@extends('layouts.app')
+@section('title')
+Portfolio Leonardo Rinaldi | Progetti 
+@endsection
+@section('content')
+<div class="container">
+    <div class="row">
+        @forelse ($projects as $project)
+        <div class="card col-4 text-center p-5">
+            <a href="#" class="text-decoration-none text-dark">
+                <h3>{{$project['name']}}
+                </h3>
+                <img src="{{$project['image']}}" style="width: 100%" alt="">
+                <h6><i>{{$project['short_description']}}</i>
+                </h6>
+            </a>
+        </div>
+        @empty
+            <h3>non sono presenti progetti...mi dispiace</h3>
+        @endforelse
+    </div>
+</div>
+@endsection
